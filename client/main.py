f4c44f6cf6d42e6f34bc156e5ee12b4685fc1a38
Emanuelpa <epatinov@eafit.edu.co>
2025-03-16 16:35:54 -0500

    clients auth with server

--- client/main.py
+++ client/main.py
@@ -1,30 +1,45 @@
 import requests
 
 BASE_URL = "http://localhost:8000"
+print("--------- CONNECT TO A SERVER ---------")
 
 while True:
-    print("-------------------------")
-    print("1. Crear cola")
-    print("2. Enviar mensaje")
-    print("3. Recibir mensaje")
-    print("4. Salir")
-    print("-------------------------")
-    option = input("Seleccione una opción: ")
-    if option == "1":
-        name = input("Enter the queue name to create: ")
-        response = requests.post(
-            f"{BASE_URL}/queue/create/", json={"name": name})
-        print(response.json())
-    elif option == "2":
-        queue = input("Enter the queue name: ")
-        message = input("Enter the message: ")
-        response = requests.post(f"{BASE_URL}/queue/send/",
-                                 params={"queue": queue, "message": message})
-        print(response.json())
-    elif option == "3":
-        queue = input("Ingrese el nombre de la cola: ")
-        response = requests.get(f"{BASE_URL}/queue/receive/",
-                                params={"queue": queue})
-        print(response.json())
-    elif option == "4":
-        break
+    user = input("Enter your username: ")
+    password = input("Enter your password: ")
+    response = requests.post(f"{BASE_URL}/connect/", json={"user": user, "password":password})
+
+    if response.status_code == 200:
+        connection = response.json()
+        print("Connected to server")
+        print("Token:", connection["token"])
+        token = connection["token"]
+
+        while True:
+            print("-------------------------")
+            print("1. Crear cola")
+            print("2. Enviar mensaje")
+            print("3. Recibir mensaje")
+            print("4. Salir")
+            print("-------------------------")
+
+            option = input("Seleccione una opción: ")
+                
+            if option == "1":
+                name = input("Enter the queue name to create: ")
+                response = requests.post(
+                    f"{BASE_URL}/queue/create/", json={"name": name}, params={"token": token})
+                print(response.json())
+            elif option == "2":
+                queue = input("Enter the queue name: ")
+                message = input("Enter the message: ")
+                response = requests.post(f"{BASE_URL}/queue/send/",params={"queue": queue, "message": message, "token": token})
+                print(response.json())
+            elif option == "3":
+                queue = input("Ingrese el nombre de la cola: ")
+                response = requests.get(f"{BASE_URL}/queue/receive/",
+                                        params={"queue": queue, "token": token})
+                print(response.json())
+            elif option == "4":
+                break
+    else:
+        print("Connection failed")
--- server/main.py
+++ server/main.py
@@ -1,15 +1,41 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from collections import deque
+import secrets
 
 app = FastAPI()
 queues = {}
+users = {}
+active_sessions = {}
+
+with open("users.txt", "r", encoding="utf-8") as file:
+    for line in file:
+        user, password = line.strip().split(",")
+        users[user] = password
 
 class QueueModel(BaseModel):
     name: str
 
+class AuthModel(BaseModel):
+    user: str
+    password: str
+
+@app.post("/connect/")
+def connect_client(auth: AuthModel):
+    if auth.user in users and auth.password == users[auth.user]:
+        token = secrets.token_hex(16)
+        active_sessions[token] = auth.user
+        return {"message": "Connected to server", "token": token}
+    raise HTTPException(status_code=401, detail="Invalid credentials")
+
+
+def verify_token(token: str):
+    if token not in active_sessions:
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
 @app.post("/queue/create/")
-def create_queue(queue: QueueModel):
+def create_queue(queue: QueueModel, token: str):
+    verify_token(token)
     if queue.name in queues:
         raise HTTPException(status_code=400, detail="Queue already exists")
     queues[queue.name] = deque()
@@ -18,7 +44,8 @@ def create_queue(queue: QueueModel):
 
 
 @app.post("/queue/send/")
-def send_message(queue: str, message: str):
+def send_message(queue: str, message: str, token: str):
+    verify_token(token)
     if not queue in queues:
         raise HTTPException(status_code=404, detail="Queue not found")
     queues[queue].append(message)
@@ -27,7 +54,8 @@ def send_message(queue: str, message: str):
 
 
 @app.get("/queue/receive/")
-def receive_message(queue: str):
+def receive_message(queue: str, token: str):
+    verify_token(token)
     if not queue in queues:
         raise HTTPException(status_code=404, detail="Queue not found")
     if len(queues[queue]) == 0:
