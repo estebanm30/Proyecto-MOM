8334bc8cdaa3fcaed3042998de09b503ecaeaf01
Esteban Muriel <emurielr@eafit.edu.co>
2025-04-02 16:12:33 -0500

    fix grpc 7

--- server/main.py
+++ server/main.py
@@ -16,9 +16,9 @@ atexit.register(close_connection)
 
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
-    uvicorn.run(app, host="0.0.0.0", port=port)
-    server = grpc_server.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc_server.serve(futures.ThreadPoolExecutor(max_workers=10))
     mom_pb2_grpc.add_MOMServiceServicer_to_server(grpc_server.MOMService(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+    uvicorn.run(app, host="0.0.0.0", port=port)
