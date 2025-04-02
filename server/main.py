2551d71243ce2cf3231ed3c4ab824386dc9d6bee
Esteban Muriel <emurielr@eafit.edu.co>
2025-04-02 16:00:03 -0500

    fix grpc 5

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from routes import router
 import atexit
 from zookeeper import close_connection
+import grpc_server
 
 
 app = FastAPI()
@@ -14,3 +15,4 @@ atexit.register(close_connection)
 if __name__ == "__main__":
     port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
     uvicorn.run(app, host="0.0.0.0", port=port)
+    grpc_server.serve()
