62533d0ed2bb1f4d869db4480958316edeb9a604
Esteban Muriel <emurielr@eafit.edu.co>
2025-04-08 12:11:14 -0500

    add zoo to replicatd queues 4

--- server/zookeeper.py
+++ server/zookeeper.py
@@ -7,7 +7,7 @@ load_dotenv()
 ZOOKEEPER_ADDRESS = os.getenv("ZOOKEEPER_ADDRESS")
 SERVER_ID = os.getenv("SERVER_ID")
 zk = KazooClient(hosts=ZOOKEEPER_ADDRESS)
-zk.start()  
+zk.start()
 
 port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
 SERVER_PATH = f"/servers/{SERVER_ID}"
@@ -25,17 +25,23 @@ def get_tokens():
         return [f"{child}" for child in children]
     return []
 
+
 def get_topic_server(topic_name: str):
     path = f"/mom_topics/{topic_name}"
     if zk.exists(path):
-        return zk.get(path)[0].decode() 
+        return zk.get(path)[0].decode()
     return None
+
 
 def get_queue_server(queue_name: str):
     path = f"/mom_queues/{queue_name}"
     if zk.exists(path):
-        return zk.get(path)[0].decode() 
+        return zk.get(path)[0].decode()
+    path = f"/mom_queues_replicas/{queue_name}"
+    if zk.exists(path):
+        return zk.get(path)[0].decode()
     return None
+
 
 def get_token_children(token):
     token_path = f"/tokens/{token}"
@@ -53,37 +59,37 @@ def close_connection():
     zk.stop()
     zk.close()
 
+
 def get_zk_client():
     return zk
 
 
 def get_round_robin_replica(current_server_id):
-    
-    all_servers = ["44.194.117.112:50051", "44.214.10.205:50051", "52.86.105.153:50051"]
-    
-    current_ip = current_server_id.split(':')[0] if ':' in current_server_id else current_server_id
+
+    all_servers = ["44.194.117.112:50051",
+                   "44.214.10.205:50051", "52.86.105.153:50051"]
+
+    current_ip = current_server_id.split(
+        ':')[0] if ':' in current_server_id else current_server_id
     candidates = [s for s in all_servers if not s.startswith(current_ip)]
-    
+
     if not candidates:
         return None, []
-    
-    
+
     rr_path = "/round_robin_index"
-    
+
     try:
-        
+
         if not zk.exists(rr_path):
             zk.create(rr_path, b"0")
-        
-        
+
         current_index, _ = zk.get(rr_path)
         current_index = int(current_index.decode())
         next_index = (current_index + 1) % len(candidates)
         zk.set(rr_path, str(next_index).encode())
-        
+
         return candidates[next_index], candidates
     except Exception as e:
         print(f"⚠️ Error en round robin selection: {str(e)}")
-        
-        return (candidates[0] if candidates else None), candidates
 
+        return (candidates[0] if candidates else None), candidates
